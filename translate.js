96ed28dd79fa17b03663a9311d370da58e825cf6
THEPOKOLOKO <124800047+THEPOKOLOKO@users.noreply.github.com>
2023-02-27 20:05:20 +0100

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,16 @@
+const translations = {
+    en: {
+      welcome: 'Welcome',
+      welcome_image: 'Welcome Image',
+      continue: 'CONTINUE'
+    },
+    pt: {
+      welcome: 'Bem-vindo',
+      welcome_image: 'Imagem de boas-vindas',
+      continue: 'CONTINUAR'
+    },
+    es: {
+      welcome: 'Bienvenido',
+      welcome_image: 'Imagen de bienvenida',
+      continue: 'CONTINUAR
+  
